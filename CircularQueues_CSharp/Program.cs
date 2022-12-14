340a214a69159290a608269c36e311b6159c21ad
Nur Azizah <nurrrazizahhh11@gmail.com>
2022-12-14 17:50:08 +0700

    menambahkan case 2

--- CircularQueues_CSharp/Program.cs
+++ CircularQueues_CSharp/Program.cs
@@ -138,6 +138,12 @@ namespace CircularQueues_CSharp
                                 q.insert(num);
                             }
                             break;
+                        case '2':
+                            {
+                                q.remove();
+                            }
+                            break;
+
                     }
                 }   
             }
