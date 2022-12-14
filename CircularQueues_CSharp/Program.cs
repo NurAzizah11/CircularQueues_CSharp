82bfd0a147cc232c6eb367bc391096aa9c83059c
Nur Azizah <nurrrazizahhh11@gmail.com>
2022-12-14 17:11:32 +0700

    menambahkan perulangan if dan else

--- CircularQueues_CSharp/Program.cs
+++ CircularQueues_CSharp/Program.cs
@@ -24,6 +24,25 @@ namespace CircularQueues_CSharp
             }
             /* The following statement checks whether the queue is empty. If the queue is 
                empty, then the value of the REAR and FRONT variables is set to 0.*/
+            if ( FRONT == -1)
+            {
+                FRONT = 0;
+                REAR = 0;
+            }
+            else
+            {
+                /* If REAR is at the last position of the array, then then value of
+                 * REAR is set to 0 that corresponds to the first position of the array.*/
+                if (REAR == max - 1)
+                    REAR = 0;
+                else
+                    /* If REAR is not at the last position, the its value is 
+                                        incremented by one.*/
+                    REAR = REAR + 1;
+            }
+            /* Once the position of REAR is determined, the element is added at 
+               its proper olace. */
+            queue_array[REAR] = element;
 
         }
     }
